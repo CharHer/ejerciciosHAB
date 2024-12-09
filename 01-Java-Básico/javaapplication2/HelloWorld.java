4a24c8fca7f615e0489c0f30b4dfbb1225d29b5d
CharHer <hocarlos92mail.com>
2024-12-09 17:55:58 -0600

    creacion de carpetas

---
+++ 01-Java-Básico/javaapplication2/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package javaapplication2;
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        
+        //System.out.println("Hola mundo");
+        System.out.println("Hello world");
+        
+    }
+    
+}
